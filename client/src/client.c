801e60a5b03f7e1974a56266ec1c794e18789cce
TotiGonzalez <tomgonzalez@frba.utn.edu.ar>
2024-04-01 22:42:25 +0000

    2ndo commit

--- client/src/client.c
+++ client/src/client.c
@@ -1,4 +1,5 @@
 #include "client.h"
+#include <readline/readline.h>
 
 int main(void)
 {
@@ -26,8 +27,13 @@ int main(void)
 
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
-
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE");
 	// Loggeamos el valor de config
+
+	log_info(logger, "Valor del config: %s", valor);
+	
 
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
@@ -56,14 +62,21 @@ t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
 	nuevo_logger = log_create("tp0.log", "TP0", 1, LOG_LEVEL_INFO);
-
+	if (nuevo_logger == NULL){
+		perror("Ha ocurrido un error");
+		exit(1);
+	}
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
-
+	nuevo_config = config_create("cliente.config");
+	if (nuevo_config == NULL){
+		perror("Error al cargar el config");
+		exit(1);
+	}
 	return nuevo_config;
 }
 
@@ -99,4 +112,5 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 	log_destroy(logger);
+	config_destroy(config);
 }
