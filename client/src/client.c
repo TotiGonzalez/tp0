6c16fcc5c305e4dd5e3574af4b5e6a423adb9aba
TotiGonzalez <tomgonzalez@frba.utn.edu.ar>
2024-04-01 22:12:34 +0000

    Iniciando Etapa 2

--- client/src/client.c
+++ client/src/client.c
@@ -18,7 +18,7 @@ int main(void)
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
-
+	log_info(logger, "Hola!, Soy un log");
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
@@ -55,6 +55,7 @@ int main(void)
 t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
+	nuevo_logger = log_create("tp0.log", "TP0", 1, LOG_LEVEL_INFO);
 
 	return nuevo_logger;
 }
@@ -97,4 +98,5 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	log_destroy(logger);
 }
